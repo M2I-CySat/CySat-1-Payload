3e3cf834694f8851a22870ede96f8f8dcb098cd3
Henry Shires <hcsgeek@gmail.com>
2023-08-22 10:18:32 -0500

    Moved middleman.py from CloneComm

--- Middleman SDR/middleman.py
+++ Middleman SDR/middleman.py
@@ -1,5 +1,6 @@
 import time
 import serial
+import os
 
 msg = "Hello World"
 print(msg)
@@ -13,38 +14,57 @@ print(msg)
 # Contact Steven Scheuermann for help, ultimatesteve99@gmail.com
 
 
+# Serial Setup
+ser = serial.Serial('COM3', 115200, timeout=10) # This will have to be changed once we test with the actual machine
+
+
+
+# Initial Declarations
+StartupDelay=1 #For deployment switch to 600 seconds
+MeasurementDelay=1 #For deployment this might be OBC specified, unsure
+StartupMeasurement=True
 
 
 # Waits a certain amount of time for system to warm up, ask Matt
+time.sleep(StartupDelay)
+
 
 # Checks if take measurement at startup is true or false
+if StartupMeasurement == True:
+    # If true, take measurement, if false, skip and go to additional commands. This might change to needing OBC prompt to take measurement.
+    os.system('cmd /c "python radiometer_app.py"') # The path has to be changed a bit for this to work but it might eventually work
 
-# If true, take measurement, if false, skip and go to additional commands. This might change to needing OBC prompt to take measurement.
 
-# Waits for measurment to be taken, until OBC responds with send me the measurement
+    # Waits for measurment to be taken, until OBC responds with send me the measurement
+    time.sleep(MeasurementDelay)
 
-# ---> Sends first file
+    # ---> Sends first file
+    #f = open("test.kelvin", "r")
+    #print(f.readline())
+    #ser.write('Sent %d time(s)')
 
-# Waits for OBC response
+    # Waits for OBC response
 
-# If error, try resending, but if 2 errors, send abort message and delete measurement probably
+    # Check if file exists
 
-# If success, delete measurement, step not needed if GNU radio does this automatically, unsure if overwrite deletes or overwrites
+    # If error, try resending, but if 2 errors, send abort message and delete measurement probably
 
-# ---> Sends second file
+    # If success, delete measurement, step not needed if GNU radio does this automatically, unsure if overwrite deletes or overwrites
 
-# Waits for OBC response
+    # ---> Sends second file
 
-# If error, try resending, but if 2 errors, send abort message and delete measurement probably
+    # Waits for OBC response
 
-# If success, delete measurement, step not needed if GNU radio does this automatically, unsure if overwrite deletes or overwrites
+    # If error, try resending, but if 2 errors, send abort message and delete measurement probably
 
-# Send "All done" message to the OBC
+    # If success, delete measurement, step not needed if GNU radio does this automatically, unsure if overwrite deletes or overwrites
 
-# If take measurement at startup is disabled, skip to here
+    # Send "All done" message to the OBC
 
-# Wait for OBC to send message for changing SDR settings
+    # If take measurement at startup is disabled, skip to here
 
-# After settings change, send "All done" message to OBC
+    # Wait for OBC to send message for changing SDR settings
 
-# After any All Done, the OBC may decide to turn the Payload off, so the program should be in a stable configuration at that point
+    # After settings change, send "All done" message to OBC
+
+    # After any All Done, the OBC may decide to turn the Payload off, so the program should be in a stable configuration at that point
